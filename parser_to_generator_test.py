8ab1d77f983edb69ad4eb8976472405220b0f1ac
Thorne Wolf <thornecanyon@gmail.com>
2021-02-28 12:26:23 -0700

    Expanding test code for SD Card files

--- parser_to_generator_test.py
+++ parser_to_generator_test.py
@@ -2,19 +2,28 @@ from utilities import UpdateSignal
 from sensor_measurement_parser import BluetoothParser
 from point_cloud_generator import PointCloudGenerator
 
+DATA_FILE = "SCAN3.TXT"
+
 
 def main():
     parser = BluetoothParser()
-    gen = PointCloudGenerator("mypointcloud")
-    with open("multi_location_data", "r") as f:
+    gen = PointCloudGenerator(DATA_FILE + "_output")
+    with open(DATA_FILE, "r") as f:
         data = f.read()
-    for i, c in enumerate(data[:]):
+    for i, c in enumerate(data):
         r = parser.add_data(c)
         if r is not None:
             gen.signal(UpdateSignal.NEW_DATA, r)
-            if r[0] == "SCAN":
-                pass
-    print(gen.scan_measurements[1])
+            # print(r)
+    l1 = gen.my_locations[1]
+    l2 = gen.my_locations[2]
+    delta = l2 - l1
+    import numpy as np
+    import math
+
+    # print(np.sqrt(delta[0]))
+    print(delta)
+    print((delta[0, 0] ** 2 + delta[0, 1] ** 2 + delta[0, 2] ** 2) ** 0.5)
 
 
 if __name__ == "__main__":
