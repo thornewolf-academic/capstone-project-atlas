47057067ce60d61cddb157b1376c02a5f4a558b2
Thorne Wolf <thornecanyon@gmail.com>
2021-03-16 16:21:03 -0700

    Updating real time viz to work within pipeline

--- real_time_visualizer.py
+++ real_time_visualizer.py
@@ -1,6 +1,6 @@
 from utilities import Subscriber, Subscribable, UpdateSignal
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib
 import csv
 from mpl_toolkits.mplot3d import Axes3D
@@ -10,10 +10,12 @@ import time
 import threading
 from multiprocessing import Process
 
+
 class RealTimeVisualizer(Subscriber):
     def __init__(self, point_cloud_file_name):
+        intvl = 1000  # ms
         self.point_cloud_file_name = point_cloud_file_name
-        self.data = np.load(self.point_cloud_file_name, allow_pickle = True)
+        self.data = np.array([[], [], []])
         self.minx = 0
         self.miny = 0
         self.minz = 0
@@ -25,31 +27,54 @@ class RealTimeVisualizer(Subscriber):
         self.current_time = time.time()
 
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.scat = self.ax.scatter([], [], [])  
+        self.ax = self.fig.add_subplot(111, projection="3d")
+        self.scat = self.ax.scatter([], [], [])
 
         self.num_rows = 0
 
-        p = Process(target=self.myplot, args=())
+        p = Process(target=self.myplot, args=(intvl,))
         p.start()
-        
 
-    def myplot(self):
-        self.ani = animation.FuncAnimation(self.fig, self.update_plot, blit=False, interval=100,
-            repeat=False)    
+    def myplot(self, intvl):
+        self.ani = animation.FuncAnimation(
+            self.fig, self.update_plot, blit=False, interval=intvl, repeat=False
+        )
         plt.show()
-    
+
     def update_plot(self, i):
+        try:
+            self.data = np.load(f"{self.point_cloud_file_name}.npy", allow_pickle=True)
+        except Exception as e:
+            print("could not read file")
+            print(e)
+            return
+
+        if self.data.shape[0] != self.num_rows:
+            try:
+                x = self.data[:, 0]
+                y = self.data[:, 1]
+                z = self.data[:, 2]
+                self.minx = min(self.minx, min(x[self.num_rows :]))
+                self.miny = min(self.miny, min(y[self.num_rows :]))
+                self.minz = min(self.minz, min(z[self.num_rows :]))
+                self.maxx = max(self.maxx, max(x[self.num_rows :]))
+                self.maxy = max(self.maxy, max(y[self.num_rows :]))
+                self.maxz = max(self.maxz, max(z[self.num_rows :]))
+            except Exception as e:
+                print(e)
+                return
+
+            self.num_rows = self.data.shape[0]
 
         x = self.data[:, 0]
         y = self.data[:, 1]
         z = self.data[:, 2]
-        
+
         self.current_time = time.time()
         self.elapsed_time = "%.2f" % (self.current_time - self.initial_time)
         print(str(self.elapsed_time) + "," + str(self.num_rows))
         self.last_time = self.current_time
-        
+
         self.ax.set_xlim(self.minx, self.maxx)
         self.ax.set_ylim(self.miny, self.maxy)
         self.ax.set_zlim(self.minz, self.maxz)
@@ -65,35 +90,36 @@ class RealTimeVisualizer(Subscriber):
         iteration = " Iteration: " + str(i)
         self.fig.suptitle(time_elapsed + numpoints + iteration)
 
-        #needs update to use animation
-        # f = r"C:\Users\victo\OneDrive\Desktop\2020-2021\SPR21\AE445 Detail\real_time.gif" 
-        # writergif = animation.PillowWriter(fps=10) 
+        # needs update to use animation
+        # f = r"C:\Users\victo\OneDrive\Desktop\2020-2021\SPR21\AE445 Detail\real_time.gif"
+        # writergif = animation.PillowWriter(fps=10)
         # ani.save(f, writer=writergif)
 
     def signal(self, signal: UpdateSignal, data=None):
         try:
-            self.data = np.load(self.point_cloud_file_name, allow_pickle = True)
+            self.data = np.load(self.point_cloud_file_name, allow_pickle=True)
         except:
             return
-        
+
         if self.data.shape[0] != self.num_rows:
             try:
                 x = self.data[:, 0]
                 y = self.data[:, 1]
                 z = self.data[:, 2]
-                self.minx = min(self.minx, min(x[self.num_rows:]))
-                self.miny = min(self.miny, min(y[self.num_rows:]))
-                self.minz = min(self.minz, min(z[self.num_rows:]))
-                self.maxx = max(self.maxx, max(x[self.num_rows:]))
-                self.maxy = max(self.maxy, max(y[self.num_rows:]))
-                self.maxz = max(self.maxz, max(z[self.num_rows:]))
+                self.minx = min(self.minx, min(x[self.num_rows :]))
+                self.miny = min(self.miny, min(y[self.num_rows :]))
+                self.minz = min(self.minz, min(z[self.num_rows :]))
+                self.maxx = max(self.maxx, max(x[self.num_rows :]))
+                self.maxy = max(self.maxy, max(y[self.num_rows :]))
+                self.maxz = max(self.maxz, max(z[self.num_rows :]))
             except Exception as e:
                 print(e)
                 return
 
             self.num_rows = self.data.shape[0]
 
-        return signal    
+        return signal
 
-if __name__ == '__main__':
-    RealTimeVisualizer('sample_point_data.npy')
+
+if __name__ == "__main__":
+    RealTimeVisualizer("sample_point_data.npy")
