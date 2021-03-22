caedf8ffd274b17f247d7a636af4909d0ef8c50a
Thorne Wolf <thornecanyon@gmail.com>
2021-03-22 13:51:51 -0700

    Adding name main statement to primary_driver.py

--- primary_driver.py
+++ primary_driver.py
@@ -9,12 +9,12 @@ from mesh_generator import MeshGenerator
 # Comments are nominal values for Henry's machine
 # Updated paths have input, output, and scripts in Project ATLAS directory
 
-POINT_CLOUD_FILE_NAME = '\scan3.xyz'                            #r"\TestMesh.xyz"
-POINT_CLOUD_FILE_LOCATION = ATLAS_DIR + '\point_cloud_in'       #r"C:\Users\Henry\PC_Input"
-MESH_FILE_NAME = '\mesh3.obj'                                   #r"\OutMesh.obj"
-MESH_FILE_LOCATION = ATLAS_DIR + '\mesh_out'                    #r"C:\Users\Henry\PC_Output"
-SCRIPT_FILE_NAME = '\script1.mlx'                               #r"\BunnyPoisson.mlx"
-SCRIPT_FILE_LOCATION = ATLAS_DIR + '\scripts'                   #r"C:\Users\Henry"
+POINT_CLOUD_FILE_NAME = "\scan3.xyz"  # r"\TestMesh.xyz"
+POINT_CLOUD_FILE_LOCATION = ATLAS_DIR + "\point_cloud_in"  # r"C:\Users\Henry\PC_Input"
+MESH_FILE_NAME = "\mesh3.obj"  # r"\OutMesh.obj"
+MESH_FILE_LOCATION = ATLAS_DIR + "\mesh_out"  # r"C:\Users\Henry\PC_Output"
+SCRIPT_FILE_NAME = "\script1.mlx"  # r"\BunnyPoisson.mlx"
+SCRIPT_FILE_LOCATION = ATLAS_DIR + "\scripts"  # r"C:\Users\Henry"
 
 
 def main():
@@ -46,3 +46,7 @@ def main():
         time.sleep(1)
 
     logger.info("Completed all ATLAS software :)")
+
+
+if __name__ == "__main__":
+    main()
