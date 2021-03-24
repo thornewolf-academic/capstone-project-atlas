dc6ae98de43288ada96f0654258355193839509b
carlyv <78232450+carlyv@users.noreply.github.com>
2021-03-24 12:51:26 -0700

    Add files via upload

--- TDLS.py
+++ TDLS.py
@@ -32,7 +32,7 @@ def TDLS(X,Y,data):
     print('3DLS progerss:')
     bar.start()
 
-    for y in range (10):
+    for y in range (0,10):
         bar.update(y+1)
         if y == 0:
             search_surface = np.zeros(len(data))
@@ -50,14 +50,36 @@ def TDLS(X,Y,data):
     scaling = np.max(data)/np.max(fun)
     filt = scaling*fun
     TDLS = np.array([None] * len(fun),dtype=np.float64)
-    for u in range(len(filt)):
+    for u in range(0,len(X)):
         bar.update(u+1)
-        for v in range(len(filt)):
+        for v in range(0,len(Y)):
             if np.abs(X[u] - X[v]) <= 0.1:
                 TDLS[u] = (filt[u]+filt[v])/2
+                TDLS[v] = (filt[u]+filt[v])/2
+                #print('LOCALIZATION VARIATION DETECTED AND ADJUSTED')
             elif np.abs(Y[u] - Y[v]) <= 0.1:
                 TDLS[u] = (filt[u]+filt[v])/2
+                TDLS[v] = (filt[u]+filt[v])/2
+                #print('LOCALIZATION VARIATION DETECTED AND ADJUSTED')
+            elif np.abs(Y[u] - X[v]) <= 0.1:
+                TDLS[u] = (filt[u]+filt[v])/2
+                TDLS[v] = (filt[u]+filt[v])/2
+                #print('LOCALIZATION VARIATION DETECTED AND ADJUSTED')
+            elif np.abs(X[u] - Y[v]) <= 0.1:
+                TDLS[u] = (filt[u]+filt[v])/2
+                TDLS[v] = (filt[u]+filt[v])/2
+                #print('LOCALIZATION VARIATION DETECTED AND ADJUSTED')
             else:
                 TDLS[u] = filt[u]
+    for i in range(5,len(TDLS)-5):
+        nearby_points = np.array([TDLS[i-5],TDLS[i-4],TDLS[i-3],TDLS[i-2],TDLS[i-1],TDLS[i],TDLS[i+1],TDLS[i+2],TDLS[i+3],TDLS[i+4],TDLS[i+5]])
+        mean_nearby_points = np.mean(nearby_points)
+        if TDLS[i] >= 1.5*mean_nearby_points:
+            TDLS[i] = np.mean(TDLS)
+            print('OUTLIER DETECTED AND ADJUSTED')
+        elif TDLS[i] <= 1.2*(np.mean(TDLS)):
+            TDLS[i] = np.mean(TDLS)
+        else:
+            TDLS[i] = TDLS[i]
     bar.finish()
     return TDLS
--- data_filterer.py
+++ data_filterer.py
@@ -1,2 +1,68 @@
+import numpy as np
+from uncertainties import unumpy as unp
+from uncertainties import umath
+import numpy as np
+import matplotlib.pyplot as plt 
+import csv
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.integrate as integrate
+import scipy.special as special
+import FWOCFM 
+import TDLS
+import winsound
+
 class DataFilterer:
-    pass
+    def __init__(self,data_in_path,filt_point_cloud_path,uncertainty_path):
+        #self.data_in_path = data_in_path
+        self.finished = 0
+        self.data = np.load(data_in_path)
+        self.filt_point_cloud_path = filt_point_cloud_path
+        self.uncertainty_path = uncertainty_path
+
+    def begin(self):
+        self.filt_data = self.filtering()
+        np.save(self.filt_point_cloud_path,self.filt_data)
+        self.error = self.uncertainty()
+        np.save(self.uncertainty_path,self.error)
+
+    def filtering(self):
+        rows = []
+        for row in self.data:
+            rows.append(row)
+        X = [float(row[0]) for row in rows]
+        Y = [float(row[1]) for row in rows]
+        Z = [float(row[2]) for row in rows]
+        X_n = np.array(X)
+        Y_n = np.array(Y)
+        Z_n = np.array(Z)
+
+        first_pass_data = FWOCFM.FWOCFM(Z_n)
+
+        second_pass_data = TDLS.TDLS(X_n,Y_n,first_pass_data)
+
+        filt_data = [X, Y, second_pass_data]
+
+        winsound.Beep(1000, 750)
+        print('DATA FILTER COMPLETE')
+
+        return filt_data
+
+    def uncertainty(self):
+        rows = []
+        for row in self.data:
+            rows.append(row)
+        X = [float(row[0]) for row in rows]
+        Y = [float(row[1]) for row in rows]
+        Z = [float(row[2]) for row in rows]
+        X_n = np.array(X)
+        Y_n = np.array(Y)
+        Z_n = np.array(Z)
+
+        u_x = std_devs(X_n)
+        u_y = std_devs(Y_n)
+        u_z = np.abs(filt_data[:,3]-Z_n + std.devs(Z_n))
+        uncertainty = np.append(u_x,u_y)
+        uncertainty = np.append(uncertainty,u_z)
+
+        return uncertainty
+
