3bbf8b4b7c24eb267dffd366ec800c33c9a4901f
Thorne Wolf <thornecanyon@gmail.com>
2021-04-13 17:15:43 -0700

    fixing real_time_visualizer bug

--- real_time_visualizer.py
+++ real_time_visualizer.py
@@ -14,6 +14,7 @@ import logging
 
 class RealTimeVisualizer(Subscriber):
     def __init__(self, file_dict):
+        self.file_dict = file_dict
 
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
